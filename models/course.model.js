61ca14284d026c1ba7080f0944c0edbf9eadc124
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-03-04 19:10:41 -0300

    AGregando pre 2

--- models/course.model.js
+++ models/course.model.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  topics: {
-    type: Array,
-    default: []
-  },
-  professor: String
+    title: String,
+    description: String,
+    topics: {
+        type: Array,
+        default: []
+    },
+    professor: String
 })
 
 const Course = mongoose.model("Course", courseSchema);
